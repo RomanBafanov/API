4f4fd5eff90246dc66e2dc8c9425a02502fa99fc
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-06-27 16:47:40 +0300

    changed main.py weather.py .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from weather import go_weather
+from weather import req_weather
 
-def show_weather():
-    go_weather()
+
+url = 'http://wttr.in'
+locations = [
+        {'san': ['francisco', 'n', 'qu', 'lang=en',]},
+        {'svo': ''},
+        {'London': ''},
+        {'Череповец': ['M', 'nmq', 'T', 'lang=ru',]},
+    ]
+
+def main():
+    for city in locations:
+        req_weather(url, city)
 
 
 if __name__ == '__main__':
-    show_weather()
+    main()
 
 
--- weather.py
+++ weather.py
@@ -1,18 +1,8 @@
 import requests
 
+def req_weather(Url, city):
+        response = requests.get(Url, params=city)
+        print(res_weather(response))
 
-url = 'http://wttr.dvmn.org'
-location = [
-        'san%20francisco?nTqu&lang=en',
-        'svo',
-        'London',
-        'Череповец?M&n&q&T&lang=ru',
-    ]
-
-def go_weather():
-    for param in location:
-        response = requests.get(url, params=param)
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print(response.raise_for_status)
+def res_weather(response):
+    return response.text if response.ok else response.raise_for_status
